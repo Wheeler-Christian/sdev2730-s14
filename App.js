20545fb8e97b78f9a38944db0329b4075b31654d
Wheeler-Christian <christianwheeler505@gmail.com>
2023-11-30 13:46:36 -0700

    Finish project s14

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.rowOfSquares}>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="airplane" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="football" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="lock-closed" size={50} color="#ccc" />
+          </View>
+        </View>
+      </View>
+      <View style={styles.rowOfSquares}>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="logo-stackoverflow" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="rocket" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <Ionicons name="moon" size={50} color="#ccc" />
+          </View>
+        </View>
+      </View>
+      <View style={styles.rowOfSquares}>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <MaterialCommunityIcons name="calculator" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <MaterialCommunityIcons name="headphones" size={50} color="#ccc" />
+          </View>
+        </View>
+        <View style={styles.outerSquare}>
+          <View style={styles.innerSquare}>
+            <MaterialCommunityIcons name="trumpet" size={50} color="#ccc" />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +67,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#222",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  rowOfSquares: {
+    flexDirection: "row",
+  },
+  outerSquare: {
+    backgroundColor: "black",
+    height: 100,
+    width: 100,
+    borderWidth: 5,
+    borderColor: "#bbb",
+    margin: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  innerSquare: {
+    height: 50,
+    width: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+    color: "white",
   },
 });
